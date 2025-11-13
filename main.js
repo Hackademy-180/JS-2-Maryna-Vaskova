399b255f020d265c3eb0a0e97dd941e4593bcba2
unknown <rinaforad@gmail.com>
2025-11-13 15:46:04 +0100

    added 1 exercize

---
+++ main.js
@@ -0,0 +1,17 @@
+// ESERCIZIO 1
+let voto = 30;
+
+if(voto < 18){
+    console.log("insufficiente");
+}else if(voto >= 18 && voto < 21){
+    console.log("sufficiente"); 
+}else if(voto >= 24 && voto < 27){
+console.log("buono");
+}else if(voto >= 27 && voto <= 29){
+    console.log("ottimo");
+}else if(voto = 30){
+    console.log("eccelente");
+}else{
+    console.log("non hai nessun voto");
+    
+}
