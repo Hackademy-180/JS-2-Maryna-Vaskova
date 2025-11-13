0d4481a1cf35fa12091de27dc66f89abbba8f024
unknown <rinaforad@gmail.com>
2025-11-13 17:34:45 +0100

    added exercize 2, 3, 4

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 // ESERCIZIO 1
-let voto = 30;
+
+let voto = 29;
 
 if(voto < 18){
     console.log("insufficiente");
@@ -11,7 +12,51 @@ console.log("buono");
     console.log("ottimo");
 }else if(voto = 30){
     console.log("eccelente");
-}else{
-    console.log("non hai nessun voto");
-    
 }
+
+// ESERCIZIO 2
+
+// let temperatura = prompt("Che tempo fa oggi?")
+
+// if(temperatura < -10){
+//     console.log("copriti…ancora ti raffreddi");
+// }else if(temperatura < 0){
+//     console.log("non è tanto il freddo quanto l’umidità");
+// }else if(temperatura < 20){
+//     console.log("non ci sono più le mezze stagioni"); 
+// }else if(temperatura >= 30){
+//     console.log("lu mare, lu sule e lu ientu"); 
+// }else if(temperatura < 30){
+//     console.log("mi dia una peroni sudata");
+// }
+
+// ESERCIZIO 3
+
+let num = 2;
+
+for(let i = 1; i <= 10; i++){
+  console.log(num + " x " + i + " = " + (num * i));
+}
+
+// ESERCIZIO 4
+
+let bevande;
+
+do{
+  bevande = prompt("Seleziona un prodotto: Tasto 1 - Acqua, Tasto 2 - Coca Cola, Tasto 3 - Birra");
+
+  switch(bevande){
+    case "1":
+      console.log("Hai selezionato l'acqua");
+      break;
+    case "2":
+      console.log("Hai selezionato la Coca Cola");
+      break;
+    case "3":
+      console.log("Hai selezionato la birra");
+      break;
+    default:
+      console.log("Non hai selezionato niente...");
+  }
+
+}while(bevande !== "1" && bevande !== "2" && bevande !== "3");
